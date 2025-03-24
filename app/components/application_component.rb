7b4f99e85c5fdd6fe0bc2a9ad0eb88df7c240643
Jerome Sadou <jerome@ohayostudio.com>
2025-03-24 18:56:48 +0900

    add video component

---
+++ app/components/application_component.rb
@@ -0,0 +1,14 @@
+# app/components/application_component.rb
+class ApplicationComponent < ActionView::Base
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+    include ActiveModel::Validations
+
+    def render_in(view_context, &block)
+        # Rails 8 component rendering logic
+        # This might already be handled by Rails 8
+        #
+
+        # Rails 7 component rendering logic
+    end
+end
