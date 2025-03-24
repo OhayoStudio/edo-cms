08f9eb4e62d5e998ea43b2d224059464d992483e
Jerome Sadou <jerome@ohayostudio.com>
2025-03-24 18:58:11 +0900

    add about route and view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :articles
   get "pinterest/code" => "pinterest#code"
   get "pinterest/callback" => "pinterest#callback"
+  get "about" => "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
